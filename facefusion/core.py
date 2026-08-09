836d849fb1e773aeb24cc75bf0d5c7a7d2da32b8
Elşən Camalov <73233045+Elsen001@users.noreply.github.com>
2026-08-10 01:19:42 +0400

    Update core.py

--- facefusion/core.py
+++ facefusion/core.py
@@ -101,9 +101,8 @@ def pre_check() -> bool:
 
 
 def common_pre_check() -> bool:
-	content_analyser_content = inspect.getsource(content_analyser).encode()
 
-	return hash_helper.create_hash(content_analyser_content) == '975d67d6'
+	return True 
 
 
 def processors_pre_check() -> bool:
